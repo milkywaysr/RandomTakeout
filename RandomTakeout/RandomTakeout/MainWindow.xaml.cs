6b727db10d6bd57d5f34a89b749596214c1889ae
heguanjie <heguanjie@live.cn>
2017-11-30 17:22:13 +0800

    add menuList

--- RandomTakeout/RandomTakeout/MainWindow.xaml.cs
+++ RandomTakeout/RandomTakeout/MainWindow.xaml.cs
@@ -23,5 +23,29 @@ namespace RandomTakeout
         {
             InitializeComponent();
         }
+
+        private void onBtnAdd(object sender, RoutedEventArgs e)
+        {
+            if(textboxAdd.Text.Length > 0)
+            {
+                menuList.Items.Add(textboxAdd.Text);
+                textboxAdd.Text = "";
+            }
+            else
+            {
+                MessageBox.Show("请输入菜单名");
+            }
+            
+        }
+
+        private void onBtnRemove(object sender, RoutedEventArgs e)
+        {
+            System.Collections.IList items = menuList.SelectedItems;
+            for(int i = 0; i < items.Count; i++)
+            {
+                menuList.Items.Remove(items[i]);
+            }
+            menuList.UnselectAll();
+        }
     }
 }
